8282ad86eb2f28c03c6727f04b1ed4e020ea897f
Benoit Chesneau <bchesneau@gmail.com>
2019-05-09 10:35:37 +0200

    make attachment process temporary
    attachments process shouldn't be restarted by the supervisor, so make
    them temporary. This fix the termination message.

--- src/barrel_fs_att_sup.erl
+++ src/barrel_fs_att_sup.erl
@@ -11,6 +11,7 @@ start_link() ->
 init([]) ->
   Spec = #{ id => view,
             start => {barrel_fs_att, start_link, []},
+            restart => temporary,
             type => worker },
   SupFlags = #{ strategy => simple_one_for_one },
   {ok, {SupFlags, [Spec]}}.
