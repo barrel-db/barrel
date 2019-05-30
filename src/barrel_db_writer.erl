0924438508e40a50b91e0877457266aac3556e01
Benoit Chesneau <bchesneau@gmail.com>
2019-05-30 14:05:15 +0200

    fix db writer
    don't move the timeout timer for the updates

--- src/barrel_db_writer.erl
+++ src/barrel_db_writer.erl
@@ -38,6 +38,7 @@ update_docs(Barrel, Docs, MergePolicy) ->
   ?start_span(#{ <<"log">> => <<"update docs">>,
                  <<"batch_size">> => length(Docs),
                  <<"merge_policy">> => barrel_lib:to_binary(MergePolicy) }),
+  TRef = erlang:send_after(5000, self(), update_timeout),
   Results = try
               [update_doc(Barrel, Doc, MergePolicy) || Doc <- Docs]
             after
@@ -58,7 +59,6 @@ do_update_doc(#{ name := Name } = Barrel, Doc, MergePolicy) ->
   StartTime = erlang:timestamp(),
   Record0 = barrel_doc:make_record(Doc),
   {_, Record1} = flush_attachments(Name, Record0),
-  TRef = erlang:send_after(5000, self(), update_timeout),
   jobs:run(barrel_write_queue,
            fun() ->
                update_doc_1(Barrel, Record1, MergePolicy, StartTime)
