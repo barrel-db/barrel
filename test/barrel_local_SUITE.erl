a9e735b76ac9dbb540949f8bf9f3839fd058d6a4
benoît chesneau <bchesneau@gmail.com>
2019-07-19 08:20:30 +0000

    fix barrel deletion
    make sure we delete the content using the correct range.

--- test/barrel_local_SUITE.erl
+++ test/barrel_local_SUITE.erl
@@ -19,12 +19,17 @@
 ]).
 
 -export([
-  local_doc/1
+  local_doc/1,
+  create_and_delete_db/1
 ]).
+
+-define(ITERATIONS, 1000).
+-define(DB_PREFIX, <<"local_test">>).
 
 all() ->
   [
-    local_doc
+    local_doc,
+    create_and_delete_db
   ].
 
 init_per_suite(Config) ->
@@ -63,3 +68,22 @@ local_doc(_Config) ->
   {ok, #{ <<"value">> := <<"local">> }} = barrel_local:get_doc(Barrel, <<"adoc">>),
   ok = barrel_local:delete_doc(Barrel, <<"adoc">>),
   {error, not_found} = barrel_local:get_doc(Barrel, <<"adoc">>).
+
+create_and_delete_db(Config) ->
+    barrel:create_barrel(?DB_PREFIX),
+    _ = barrel_db:open_barrel(?DB_PREFIX),
+    {ok, _ } = barrel:barrel_infos(?DB_PREFIX),
+    create_and_delete_db_loop(Config, ?ITERATIONS),
+    barrel:delete_barrel(?DB_PREFIX).
+
+create_and_delete_db_loop(_, 0) ->
+    ok;
+create_and_delete_db_loop(Config, Int) when Int > 0 ->
+    error_logger:info_msg("Starting iteration ~p of database creation and deletion",
+                          [?ITERATIONS - Int + 1]),
+    %% Do something here.
+    Name = <<(?DB_PREFIX)/binary, $_, (barrel_id:binary_id(62))/binary >>,
+    ok = barrel:create_barrel(Name),
+    ok = barrel:delete_barrel(Name),
+    {ok, _ } = barrel:barrel_infos(?DB_PREFIX),
+    create_and_delete_db_loop(Config, Int - 1).
